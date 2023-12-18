8ac1a658e21395bf11e883aa1f9375f0ac28d05c
sarke-varke <152171299+sarke-varke@users.noreply.github.com>
2023-12-18 14:40:23 +0200

    Add files via upload

---
+++ Š_Jadzgevičius-Exercises/pd4/javascriptukas.js
@@ -0,0 +1,18 @@
+//1. deklaruoju
+let petName;
+let petAge;
+//2. inicializuoju
+petName = "sarunas";
+petAge = "3";
+//3. loginu
+console.log("Pet Name:", petName);
+console.log("Pet Age:", petAge);
+//4. keičiu litrealus
+petName = "ne sarunas";
+petAge = "ne 3";
+//5.deklaruoju ir inicializuoju adresa
+const buildingAddress = "home";
+//6.loginu pakeitimus ir nauja kintamaji
+console.log("Updated Pet Name:", petName);
+console.log("Updated Pet Age:", petAge);
+console.log("Building Adress", buildingAddress);
