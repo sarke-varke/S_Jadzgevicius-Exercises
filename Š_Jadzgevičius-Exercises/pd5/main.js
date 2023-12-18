30e969beed43d7682dbefafd5c9fc3812fd00c26
sarke-varke <152171299+sarke-varke@users.noreply.github.com>
2023-12-18 15:42:59 +0200

    Add files via upload

---
+++ Š_Jadzgevičius-Exercises/pd5/main.js
@@ -0,0 +1,26 @@
+//1-7 kintamieji
+let numberExample = 42; //1.
+let bigintExample = BigInt(9007199254740991); //2.
+let stringExample = "Hello, World!"; //3.
+let booleanExample = true; //4.
+let undefinedExample; //5.
+let nullExample = null; //6.
+let objectExample = { key: "value" }; //7.
+
+//1-7 logai
+console.log("Number Example:", numberExample); //1.
+console.log("Bigint Example:", bigintExample); //2.
+console.log("String Example:", stringExample); //3.
+console.log("Boolean Example:", booleanExample); //4.
+console.log("Undefined Example:", undefinedExample); //5.
+console.log("Null Example:", nullExample); //6.
+console.log("Object Example:", objectExample); //7.
+
+//8.typeoff
+console.log("Type of numberExample:", typeof numberExample); //1.
+console.log("Type of bigintExample:", typeof bigintExample); //2.
+console.log("Type of stringExample:", typeof stringExample); //3.
+console.log("Type of booleanExample:", typeof booleanExample); //4.
+console.log("Type of undefinedExample:", typeof undefinedExample); //5.
+console.log("Type of nullExample:", typeof nullExample); //6.
+console.log("Type of objectExample:", typeof objectExample); //7.
