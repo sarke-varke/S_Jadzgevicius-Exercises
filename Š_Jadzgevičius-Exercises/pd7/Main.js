bce0deab3213d8c2479eaf125698f9103dde21fe
sarke-varke <152171299+sarke-varke@users.noreply.github.com>
2024-01-25 00:55:29 +0200

    Add files via upload

---
+++ Š_Jadzgevičius-Exercises/pd7/Main.js
@@ -0,0 +1,17 @@
+let pirmas = 10;
+let antras = 20;
+
+console.log(pirmas + antras); 
+console.log(pirmas - antras); 
+console.log(pirmas * antras); 
+console.log(pirmas / antras); 
+console.log(pirmas % antras); 
+console.log(pirmas ** antras); 
+console.log(pirmas++);
+console.log(++pirmas); 
+console.log(pirmas--); 
+console.log(--pirmas); 
+console.log(--pirmas + antras++); 
+console.log(pirmas-- - --antras + antras++); 
+console.log(pirmas++ / antras++); 
+console.log(pirmas++ * --antras); 
